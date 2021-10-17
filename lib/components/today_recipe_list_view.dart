9ac828ee826e70c957259c3cb42352d1d5b4ecff
FlutLab.io <>
2021-10-17 20:11:34 +0000

    Autosave

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -55,7 +55,7 @@ class TodayRecipeListView extends StatelessWidget {
               },
             ),
 
-            color: Colors.grey,
+            // color: Colors.grey,
           ),
         ],
       ),
